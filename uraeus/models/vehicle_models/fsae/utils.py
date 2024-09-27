58021b6d5cbc55b8cc420410318ee6565c982b50
khaledghobashy <khaled.ghobashy@live.com>
2024-09-27 19:58:08 +0000

    minor changes in custom joints polynomials

--- uraeus/models/vehicle_models/fsae/utils.py
+++ uraeus/models/vehicle_models/fsae/utils.py
@@ -33,7 +33,7 @@ def right_pose_polynomials(qdt0: np.ndarray) -> np.ndarray:
     x = (2.5462e-5 * z**2) + (-7.98277e-2 * z) + (-3.3937e-3)
     y = (1.3162e-5 * z**3) + (-1.2945e-3 * z**2) + (1.8655e-1 * z**1) + (1.475e-2)
 
-    pose_states = jnp.array([x * 1e-3, -y * 1e-3, z * 1e-3, phi, theta, psi])
+    pose_states = jnp.array([x * 1e-3, -y * 1e-3, z * 1e-3, 0, 0, 0])
     return pose_states
 
 
@@ -65,7 +65,7 @@ def left_pose_polynomials(qdt0: np.ndarray) -> np.ndarray:
     x = (2.5462e-5 * z**2) + (-7.98277e-2 * z) + (-3.3937e-3)
     y = (1.3162e-5 * z**3) + (-1.2945e-3 * z**2) + (1.8655e-1 * z**1) + (1.475e-2)
 
-    pose_states = jnp.array([x * 1e-3, y * 1e-3, z * 1e-3, phi, theta, psi])
+    pose_states = jnp.array([x * 1e-3, y * 1e-3, z * 1e-3, 0, 0, 0])
     return pose_states
 
 
