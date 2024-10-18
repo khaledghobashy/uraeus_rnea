df4d86a9e775226a544971d0b5b48a2b174cf00f
khaledghobashy <khaled.ghobashy@live.com>
2024-10-18 15:49:23 +0000

    - added more helper functions - modified `Model.ssode` to use `get_qdt1_from_udt0`

--- uraeus/rnea/topologies.py
+++ uraeus/rnea/topologies.py
@@ -1,6 +1,8 @@
 from functools import reduce
-from typing import Callable
+from typing import Callable, Any
+from collections import namedtuple
 
+import jax.numpy as jnp
 import numpy as np
 
 from uraeus.rnea.bodies import RigidBody, RigidBodyData, BodyKinematics
@@ -15,6 +17,8 @@ from uraeus.rnea.joints import (
     construct_joint_instance,
     initialize_joint,
     JointKinematics,
+    TranslationalJoint,
+    RevoluteJoint,
 )
 
 from uraeus.rnea.algorithms import (
@@ -25,6 +29,7 @@ from uraeus.rnea.algorithms import (
     IDCallRes,
     inverse_dynamics_call,
     forward_dynamics_call,
+    get_qdt1_from_udt0,
 )
 
 Forcesdict = dict[str, dict[str, dict[str, np.ndarray]]]
@@ -134,7 +139,7 @@ class MultiBodyTree(object):
         return self.bodies[name]
 
     def construct_bodies_coordinates_names(self) -> list[str]:
-        coordinates = ["phi", "theta", "psi", "x", "y", "z"]
+        coordinates = ["x", "y", "z", "phi", "theta", "psi"]
         namer = lambda l, name: l + [f"{name}.{c}" for c in coordinates]
         bodies_coordinates = reduce(namer, self.bodies.keys(), [])
         return bodies_coordinates
@@ -151,7 +156,7 @@ def construct_permutation_matrix(dof: int, id_indices: list[int]) -> np.ndarray:
     return mat
 
 
-def construct_hybriddynamics_data(
+def construct_hybrid_dynamics_data(
     tree_data: MultiBodyData, id_coordinates: np.ndarray
 ) -> HybridDynamicsData:
     n_dof = sum([j.nj for j in tree_data.joints])
@@ -167,12 +172,29 @@ def construct_hybriddynamics_data(
     return data
 
 
-def construct_multibodydata(topology: MultiBodyTree) -> MultiBodyData:
+def create_namedtuple_inplace(name: str, field_dict: dict[str, Any]) -> namedtuple:
+    return namedtuple(name, field_dict.keys())(*field_dict.values())
+
+
+def construct_multibody_data(topology: MultiBodyTree) -> MultiBodyData:
     func_joints = tuple(map(construct_functional_joint, topology.joints.values()))
     bodies_inertias = [b.I for b in topology.bodies.values()]
     forward_traversal, backward_traversal = construct_traversal_orders(topology.tree)
     qdt0_idx = [0] + list(np.cumsum([j.nj for j in func_joints]))
     qdt1_idx = qdt0_idx  # Equal each other for now. Later could be different.
+
+    states_tuple = namedtuple("States", topology.joints.keys())
+    qdt0_names = states_tuple(
+        **{
+            name: create_namedtuple_inplace(
+                name, dict(zip(joint.joint_type.coordinates_names, indices))
+            )
+            for (name, joint), indices in zip(
+                topology.joints.items(),
+                np.split(np.arange(0, topology.dof), qdt0_idx)[1:],
+            )
+        },
+    )
 
     data = MultiBodyData(
         joints=func_joints,
@@ -181,6 +203,7 @@ def construct_multibodydata(topology: MultiBodyTree) -> MultiBodyData:
         backward_traversal=backward_traversal,
         qdt0_idx=tuple(qdt0_idx),
         qdt1_idx=tuple(qdt1_idx),
+        qdt0_names=qdt0_names,
     )
 
     return data
@@ -199,8 +222,16 @@ class Model(object):
             for b in self.topology.bodies.values()
         }
 
-        self.tree_data = construct_multibodydata(topology)
+        self.tree_data = construct_multibody_data(topology)
         self.bodies_idx = {b: i for i, b in enumerate(self.topology.tree.nodes)}
+
+    @property
+    def n(self):
+        return self.dof
+
+    @property
+    def dof(self):
+        return self.topology.dof
 
     def get_body_kinematics(
         self, name: str, bodies_kinematics: list[BodyKinematics]
@@ -232,16 +263,12 @@ class Model(object):
         qdt2 = forward_dynamics_call(self.tree_data, forces, qdt0, qdt1, tau)
         return qdt2
 
-    def ssode(
-        self,
-        t: float,
-        ydt0: np.ndarray,
-        forces_func: Callable,
-    ):
-        qdt0, qdt1 = ydt0.reshape(2, -1)
-        tau, self.forces_map = forces_func(self, qdt0, qdt1, 0 * qdt1, t)
-        qdt2 = self.forward_dynamics_pass(qdt0, qdt1, tau)
-        return np.hstack([qdt1, qdt2])
+    def ssode(self, t: float, ydt0: np.ndarray, forces_func: Callable, *args):
+        qdt0, udt0 = ydt0.reshape(2, -1)
+        tau, self.forces_map = forces_func(self, qdt0, udt0, np.zeros(udt0.shape), t)
+        udt1 = self.forward_dynamics_pass(qdt0, udt0, tau)
+        qdt1 = get_qdt1_from_udt0(self.tree_data, qdt0, udt0)
+        return np.hstack([qdt1, udt1])
 
 
 class HybridModel(Model):
@@ -250,9 +277,14 @@ class HybridModel(Model):
 
     def __init__(self, topology: MultiBodyTree, id_coordinates: np.ndarray):
         super().__init__(topology)
-        self.hybrid_dynamics_data = construct_hybriddynamics_data(
+        self.id_coordinates = id_coordinates
+        self.hybrid_dynamics_data = construct_hybrid_dynamics_data(
             self.tree_data, id_coordinates
         )
+
+    @property
+    def n(self):
+        return self.dof - len(self.id_coordinates)
 
     def forward_dynamics_call(
         self,
@@ -273,32 +305,81 @@ class HybridModel(Model):
         return qdt2_fd
 
     def ssode(
-        self, t, ydt0, forces_func: Callable, motion_func: Callable
+        self,
+        t: float,
+        ydt0_fd: np.ndarray,
+        u: dict[str, float],
+        kinematic_actuation: Callable,
+        dynamics_actuation: Callable,
     ) -> np.ndarray:
 
-        qdt0_fd, qdt1_fd = ydt0.reshape(2, -1)
-        qdt2_fd = 0 * qdt1_fd
-        qdt0_id, qdt1_id, qdt2_id = motion_func(t)
-
         n_fd = self.hybrid_dynamics_data.n_fd
+        qdt0_fd, qdt1_fd = ydt0_fd.reshape(2, -1)
+        qdt2_fd = np.zeros((n_fd,))
 
-        qdt0 = self.hybrid_dynamics_data.permutation_matrix.T @ np.array(
-            [*qdt0_fd, *qdt0_id]
-        )
-        qdt1 = self.hybrid_dynamics_data.permutation_matrix.T @ np.array(
-            [*qdt1_fd, *qdt1_id]
-        )
-        qdt2 = self.hybrid_dynamics_data.permutation_matrix.T @ np.array(
-            [*qdt2_fd, *qdt2_id]
-        )
+        # Evaluating the inverse-dynamics coordinates using the given
+        # kinematic_actuation function.
+        dt0_id, qdt1_id, qdt2_id = kinematic_actuation(self, t, ydt0_fd, u)
 
-        tau, self.forces_map = forces_func(self, qdt0, qdt1, qdt2, t)
+        # Constructing a new system-state, containing both inverse-dynamics and
+        # forward-dynamics joints states, for routines which need the full system
+        # state
+        qdt0, qdt1, qdt2 = reconstruct_system_coordinates(
+            self, (qdt0_fd, qdt1_fd, qdt2_fd), (dt0_id, qdt1_id, qdt2_id)
+        )
+        ydt0 = np.hstack([qdt0, qdt1])
 
+        # Evaluate applied external-forces using the provided callable
+        tau, self.forces_map = dynamics_actuation(self, t, ydt0, u)
+
+        # Extracting the forward-dynamics generalized forces vector.
         tau_fd = (self.hybrid_dynamics_data.permutation_matrix @ tau)[:n_fd]
 
         qdt2_fd = self.forward_dynamics_call(qdt0, qdt1, qdt2_id, tau_fd)
-        ydt2 = np.array([*qdt1_fd, *qdt2_fd])
-        return ydt2
+
+        # extracting qdt1 from udt0, assuming qdt1 != udt0
+        qdt0, qdt1, qdt2 = reconstruct_system_coordinates(
+            self, (qdt0_fd, qdt1_fd, qdt2_fd), (dt0_id, qdt1_id, qdt2_id)
+        )
+        qdt1 = get_qdt1_from_udt0(self.tree_data, qdt0, qdt1)
+
+        ydt1 = permute_state_coordinates(
+            self.hybrid_dynamics_data.permutation_matrix,
+            qdt1,
+            qdt2,
+            self.hybrid_dynamics_data.n_fd,
+        )
+        return ydt1
+
+
+def permute_state_coordinates(
+    permutation_matrix: np.ndarray, qdt0: np.ndarray, qdt1: np.ndarray, n_fd: int
+):
+    qdt0_permuted = permutation_matrix @ qdt0
+    qdt1_permuted = permutation_matrix @ qdt1
+    ydt0 = np.hstack([qdt0_permuted[:n_fd], qdt1_permuted[:n_fd]])
+    return ydt0
+
+
+def reconstruct_system_coordinates(
+    model: HybridModel,
+    dynamic_coordinates,
+    kinematic_coordinates,
+):
+    qdt0_fd, qdt1_fd, qdt2_fd = dynamic_coordinates
+    qdt0_id, qdt1_id, qdt2_id = kinematic_coordinates
+
+    qdt0 = model.hybrid_dynamics_data.permutation_matrix.T @ np.array(
+        [*qdt0_fd, *qdt0_id]
+    )
+    qdt1 = model.hybrid_dynamics_data.permutation_matrix.T @ np.array(
+        [*qdt1_fd, *qdt1_id]
+    )
+    qdt2 = model.hybrid_dynamics_data.permutation_matrix.T @ np.array(
+        [*qdt2_fd, *qdt2_id]
+    )
+
+    return qdt0, qdt1, qdt2
 
 
 def _convert_body_forces_dict_to_list(forces_dict: dict[str, dict[str, np.ndarray]]):
@@ -315,5 +396,156 @@ def construct_system_forces_from_dict(external_forces: Forcesdict) -> SystemForc
         )
         for forces_dict in external_forces.values()
     ]
-    # print(system_forces)
     return system_forces
+
+
+def emulate_free_joint_v1(
+    model: MultiBodyTree,
+    predecessor: str,
+    successor: str,
+    succ_data: RigidBodyData,
+    joint_config: JointConfigInputs,
+) -> MultiBodyTree:
+    dummy_body_data = RigidBodyData()
+
+    x_axis_config = JointConfigInputs(
+        pos=joint_config.pos, z_axis=np.array([1, 0, 0]), x_axis=None
+    )
+    y_axis_config = JointConfigInputs(
+        pos=joint_config.pos, z_axis=np.array([0, 1, 0]), x_axis=None
+    )
+    z_axis_config = JointConfigInputs(
+        pos=joint_config.pos, z_axis=np.array([0, 0, 1]), x_axis=None
+    )
+
+    model.add_joint(
+        joint_name="x_trans",
+        predecessor=predecessor,
+        successor="d1",
+        succ_data=dummy_body_data,
+        joint_type=TranslationalJoint,
+        joint_data=x_axis_config,
+    )
+
+    model.add_joint(
+        joint_name="y_trans",
+        predecessor="d1",
+        successor="d2",
+        succ_data=dummy_body_data,
+        joint_type=TranslationalJoint,
+        joint_data=y_axis_config,
+    )
+
+    model.add_joint(
+        joint_name="z_trans",
+        predecessor="d2",
+        successor="d3",
+        succ_data=dummy_body_data,
+        joint_type=TranslationalJoint,
+        joint_data=z_axis_config,
+    )
+
+    model.add_joint(
+        joint_name="x_rotation",
+        predecessor="d3",
+        successor="d4",
+        succ_data=dummy_body_data,
+        joint_type=RevoluteJoint,
+        joint_data=x_axis_config,
+    )
+
+    model.add_joint(
+        joint_name="y_rotation",
+        predecessor="d4",
+        successor="d5",
+        succ_data=dummy_body_data,
+        joint_type=RevoluteJoint,
+        joint_data=y_axis_config,
+    )
+
+    model.add_joint(
+        joint_name="z_rotation",
+        predecessor="d5",
+        successor=successor,
+        succ_data=succ_data,
+        joint_type=RevoluteJoint,
+        joint_data=z_axis_config,
+    )
+
+    return model
+
+
+def emulate_free_joint_v2(
+    model: MultiBodyTree,
+    predecessor: str,
+    successor: str,
+    succ_data: RigidBodyData,
+    joint_config: JointConfigInputs,
+) -> MultiBodyTree:
+    dummy_body_data = RigidBodyData()
+
+    x_axis_config = JointConfigInputs(
+        pos=joint_config.pos, z_axis=np.array([1, 0, 0]), x_axis=None
+    )
+    y_axis_config = JointConfigInputs(
+        pos=joint_config.pos, z_axis=np.array([0, 1, 0]), x_axis=None
+    )
+    z_axis_config = JointConfigInputs(
+        pos=joint_config.pos, z_axis=np.array([0, 0, 1]), x_axis=None
+    )
+
+    model.add_joint(
+        joint_name="z_rotation",
+        predecessor=predecessor,
+        successor="d1",
+        succ_data=dummy_body_data,
+        joint_type=RevoluteJoint,
+        joint_data=z_axis_config,
+    )
+
+    model.add_joint(
+        joint_name="y_rotation",
+        predecessor="d1",
+        successor="d2",
+        succ_data=dummy_body_data,
+        joint_type=RevoluteJoint,
+        joint_data=y_axis_config,
+    )
+
+    model.add_joint(
+        joint_name="x_rotation",
+        predecessor="d2",
+        successor="d3",
+        succ_data=dummy_body_data,
+        joint_type=RevoluteJoint,
+        joint_data=x_axis_config,
+    )
+
+    model.add_joint(
+        joint_name="x_trans",
+        predecessor="d3",
+        successor="d4",
+        succ_data=dummy_body_data,
+        joint_type=TranslationalJoint,
+        joint_data=x_axis_config,
+    )
+
+    model.add_joint(
+        joint_name="y_trans",
+        predecessor="d4",
+        successor="d5",
+        succ_data=dummy_body_data,
+        joint_type=TranslationalJoint,
+        joint_data=y_axis_config,
+    )
+
+    model.add_joint(
+        joint_name="z_trans",
+        predecessor="d5",
+        successor=successor,
+        succ_data=succ_data,
+        joint_type=TranslationalJoint,
+        joint_data=z_axis_config,
+    )
+
+    return model
